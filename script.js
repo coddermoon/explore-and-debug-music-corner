8ef1c81c833cf245939eaf604b2025f81ef0ebed
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-13 20:53:14 +0600

    missing return in id

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const elementById = (id) => {
-  document.getElementById(id);
+  return document.getElementById(id);
 };
 
 const handleSearch = () => {
   const keyword = elementById("keyword");
+  
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
