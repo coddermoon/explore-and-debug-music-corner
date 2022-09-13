03921971ff2418fbb8497d0a2255124cf5439599
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-13 21:29:29 +0600

    done

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const showArtists = (data) => {
   const artistContainer = elementById("artists");
   data?.artists?.forEach((artist) => {
     
-    console.log(artistContainer)
+  
     const div = document.createElement("div");
     div.classList.add("artist-card");
     div.innerHTML = `<div class="image-container">
@@ -40,31 +40,36 @@ const showArtists = (data) => {
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  
   fetch(url)
-    .then((res) => res.JSON())
+    .then((res) => res.json())
     .then((data) => showAlbum(data));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
 };
 
 const showAlbum = (data) => {
+  
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  data?.album?.forEach((item) => {
+    console.log(item)
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${item.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
     albumContainer.appendChild(div);
   });
 };
+handleSearch()
