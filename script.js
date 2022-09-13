69a22e8d5b8bbbb4c4ab51a9c3e0652326a42246
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-13 20:59:41 +0600

    append Problem solved

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const elementById = (id) => {
 
 const handleSearch = () => {
   const keyword = elementById("keyword");
-  
+
   const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
   fetch(url)
     .then((res) => res.json())
@@ -12,27 +12,29 @@ const handleSearch = () => {
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  const artistContainer = elementById("artists");
   data?.artists?.forEach((artist) => {
+    
+    console.log(artistContainer)
     const div = document.createElement("div");
     div.classList.add("artist-card");
     div.innerHTML = `<div class="image-container">
-    <div class="image-container-inner">
-      <img
-        src="${artist.strArtistThumb}"
-        alt=""
-      />
-    </div>
-  </div>
-  <div class="info-container">
-    <h1>${artist.strArtist}</h1>
-    <p>Country: ${artist.strCountry}</p>
-    <p>Style: ${artist.strGenre}</p>
-  </div>
-  <button class="album-button">
-    <i class="fa-solid fa-compact-disc"></i>
-    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
-  </button>`;
+        <div class="image-container-inner">
+          <img
+            src="${artist.strArtistThumb}"
+            alt=""
+          />
+        </div>
+        </div>
+        <div class="info-container">
+          <h1>${artist.strArtist}</h1>
+          <p>Country: ${artist.strCountry}</p>
+          <p>Style: ${artist.strGenre}</p>
+        </div>
+        <button class="album-button">
+          <i class="fa-solid fa-compact-disc"></i>
+          <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+        </button>`;
     artistContainer.appendChild(div);
   });
 };
